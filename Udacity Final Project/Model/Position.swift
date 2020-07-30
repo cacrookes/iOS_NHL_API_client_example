2fa91146170fda29661e438a023605e7b30f7e0b
Christopher Crookes <cacrookes@gmail.com>
2020-07-30 12:28:24 -0700

    Creating separate file for Position struct since it is used in multiple responses.

---
+++ Udacity Final Project/Model/Position.swift
@@ -0,0 +1,16 @@
+//
+//  Position.swift
+//  Udacity Final Project
+//
+//  Created by Christopher Crookes on 2020-07-30.
+//  Copyright © 2020 Christopher Crookes. All rights reserved.
+//
+
+import Foundation
+
+struct Position: Codable {
+    let code: String
+    let name: String
+    let type: String
+    let abbreviation: String
+}
