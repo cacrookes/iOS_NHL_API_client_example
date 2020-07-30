47f38bffb3596376f735b1d4bb64046ca472bb79
Christopher Crookes <cacrookes@gmail.com>
2020-07-30 12:47:17 -0700

    Adding SingleSeasonStatsResponse to handle requests for a player's single season stats.

---
+++ Udacity Final Project/Model/SingleSeasonStatsResponse.swift
@@ -0,0 +1,54 @@
+//
+//  SingleSeasonStatsResponse.swift
+//  Udacity Final Project
+//
+//  Created by Christopher Crookes on 2020-07-30.
+//  Copyright © 2020 Christopher Crookes. All rights reserved.
+//
+
+import Foundation
+
+struct SingleSeasonStatsResponse: Codable {
+    struct Stats: Codable {
+        struct StatsType: Codable {
+            let displayName: String
+        }
+        struct Splits: Codable {
+            struct Stat: Codable {
+                let timeOnIce: String
+                let assists: Int
+                let goals: Int
+                let pim: Int
+                let shots: Int
+                let games: Int
+                let hits: Int
+                let powerPlayGoals: Int
+                let powerPlayPoints: Int
+                let powerPlayTimeOnIce: String
+                let evenTimeOnIce: String
+                let penaltyMinutes: String
+                let faceOffPct: Double
+                let shotPct: Int
+                let gameWinningGoals: Int
+                let overTimeGoals: Int
+                let shortHandedGoals: Int
+                let shortHandedPoints: Int
+                let shortHandedTimeOnIce: String
+                let blocked: Int
+                let plusMinus: Int
+                let points: Int
+                let shifts: Int
+                let timeOnIcePerGame: String
+                let evenTimeOnIcePerGame: String
+                let shortHandedTimeOnIcePerGame: String
+                let powerPlayTimeOnIcePerGame: String
+            }
+            let season: String
+            let stat: Stat
+        }
+        let type: StatsType
+        let splits: [Splits]
+    }
+    let copyright: String
+    let stats: Stats
+}
