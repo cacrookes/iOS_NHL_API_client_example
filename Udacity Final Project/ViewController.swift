78693d66145e2fed7f2ab089204474ca4ecae747
Christopher Crookes <cacrookes@gmail.com>
2020-07-30 14:10:28 -0700

    Testing network requests.

--- Udacity Final Project/ViewController.swift
+++ Udacity Final Project/ViewController.swift
@@ -10,10 +10,44 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    fileprivate func printTeams() {
+        
+        NHLClient.getTeamList { (teams, error) in
+            print(teams.count)
+            for team in teams{
+                print(team.name)
+            }
+        }
+    }
+    
+    fileprivate func printRoster() {
+        
+        NHLClient.getTeamRoster(forTeamID: 26) { (roster, error) in
+            print(roster.count)
+            for player in roster {
+                print(player.person.fullName)
+            }
+        }
+    }
+    
+    fileprivate func printPlayerInfo() {
+        NHLClient.getPlayerInfo(forPlayerID: 8475204) { (playerInfo, error) in
+            print(playerInfo?.fullName ?? "")
+        }
+    }
+    
+    fileprivate func printPlayerStats() {
+        NHLClient.getPlayerStats(forPlayerID: 8475204, forSeason: "20192020") { (stats, error) in
+            print(stats?.points ?? 0)
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        printPlayerStats()
     }
+    
 
 
 }
