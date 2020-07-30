5f92aadfbc20abfbfd7d80476d075426ffb95ef8
Christopher Crookes <cacrookes@gmail.com>
2020-07-30 12:21:18 -0700

    Adding RosterReponse to handle response from API roster requests.

---
+++ Udacity Final Project/Model/RosterResponse.swift
@@ -0,0 +1,31 @@
+//
+//  RosterResponse.swift
+//  Udacity Final Project
+//
+//  Created by Christopher Crookes on 2020-07-30.
+//  Copyright © 2020 Christopher Crookes. All rights reserved.
+//
+
+import Foundation
+
+struct RosterResponse: Codable {
+    struct Player: Codable {
+        struct Person: Codable {
+            let id: Int
+            let fullName: String
+            let link: String
+        }
+        struct Position: Codable {
+            let code: String
+            let name: String
+            let type: String
+            let abbreviation: String
+        }
+        let person: Person
+        let jerseyNumber: String
+        let position: Position
+    }
+    let copyright: String
+    let roster: [Player]
+    let link: String
+}
