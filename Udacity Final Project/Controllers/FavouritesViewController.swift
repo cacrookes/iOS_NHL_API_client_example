35ba9d074a57981d930bf183dc25cb178ed8b616
Christopher Crookes <cacrookes@gmail.com>
2020-07-31 12:34:48 -0700

    Wiring up FavouritePlayersTableView.

--- Udacity Final Project/Controllers/FavouritesViewController.swift
+++ Udacity Final Project/Controllers/FavouritesViewController.swift
@@ -13,58 +13,76 @@ class FavouritesViewController: UIViewController {
 
     let dataController = (UIApplication.shared.delegate as! AppDelegate).dataController
     
-//    fileprivate func printTeams() {
-//
-//        NHLClient.getTeamList { (teams, error) in
-//            let geoCoder = CLGeocoder()
-//            for team in teams{
-//                print("\(team.name) - \(team.venue.name), \(team.locationName)")
-//                let venueAddress = "\(team.venue.name), \(team.locationName)"
-////                geoCoder.geocodeAddressString(venueAddress) { (placemarks, error) in
-////                    if error != nil {
-////                        print(error!)
-////                    }
-////                    if let location = placemarks?.first?.location {
-////                        print(location.coordinate)
-////                    }
-////                }
-//
-//            }
-//        }
-//    }
-//
-//    fileprivate func printRoster() {
-//
-//        NHLClient.getTeamRoster(forTeamID: 26) { (roster, error) in
-//            print(roster.count)
-//            for player in roster {
-//                print(player.person.fullName)
-//            }
-//        }
-//    }
-//
-//    fileprivate func printPlayerInfo() {
-//        NHLClient.getPlayerInfo(forPlayerID: 8475204) { (playerInfo, error) in
-//            print(playerInfo?.fullName ?? "")
-//        }
-//    }
-//
-//    fileprivate func printPlayerStats() {
-//        NHLClient.getPlayerStats(forPlayerID: 8475204, forSeason: "20192020") { (stats, error) in
-//            print(stats?.points ?? 0)
-//        }
-//    }
+    var favouritePlayers = [Player]()
+    
+    @IBOutlet weak var favouritePlayersTableView: UITableView!
+    
+
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        let teams = dataController.fetchTeams()
-        for team in teams {
-            print("\(team.id): \(team.abbreviation!) - \(team.name!)")
-        }
+        
     }
     
-
+    
+    
 
 }
 
+extension FavouritesViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return favouritePlayers.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = favouritePlayersTableView.dequeueReusableCell(withIdentifier: K.Identifiers.favouritePlayerTableViewCell)!
+        
+        return cell
+    }
+    
+}
+
+/*
+ fileprivate func printTeams() {
+
+     NHLClient.getTeamList { (teams, error) in
+         let geoCoder = CLGeocoder()
+         for team in teams{
+             print("\(team.name) - \(team.venue.name), \(team.locationName)")
+             let venueAddress = "\(team.venue.name), \(team.locationName)"
+             geoCoder.geocodeAddressString(venueAddress) { (placemarks, error) in
+                 if error != nil {
+                     print(error!)
+                 }
+                 if let location = placemarks?.first?.location {
+                     print(location.coordinate)
+                 }
+             }
+
+         }
+     }
+ }
+
+ fileprivate func printRoster() {
+
+     NHLClient.getTeamRoster(forTeamID: 26) { (roster, error) in
+         print(roster.count)
+         for player in roster {
+             print(player.person.fullName)
+         }
+     }
+ }
+
+ fileprivate func printPlayerInfo() {
+     NHLClient.getPlayerInfo(forPlayerID: 8475204) { (playerInfo, error) in
+         print(playerInfo?.fullName ?? "")
+     }
+ }
+
+ fileprivate func printPlayerStats() {
+     NHLClient.getPlayerStats(forPlayerID: 8475204, forSeason: "20192020") { (stats, error) in
+         print(stats?.points ?? 0)
+     }
+ }
+ */
