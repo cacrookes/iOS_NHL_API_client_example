1dee6b81c48a14e8cfe87106419fafeb86c40d00
Christopher Crookes <cacrookes@gmail.com>
2020-07-31 14:44:05 -0700

    Moved PlayerInfo Struct to its own file.

--- Udacity Final Project/Model/Responses/PlayerResponse.swift
+++ Udacity Final Project/Model/Responses/PlayerResponse.swift
@@ -9,35 +9,6 @@
 import Foundation
 
 struct PlayerResponse: Codable {
-    struct PlayerInfo: Codable {
-        struct Team: Codable {
-            let id: Int
-            let name: String
-            let link: String
-        }
-        let id: Int
-        let fullName: String
-        let link: String
-        let firstName: String
-        let lastName: String
-        let primaryNumber: String
-        let birthDate: String
-        let currentAge: Int
-        let birthCity: String
-        let birthStateProvince: String
-        let birthCountry: String
-        let nationality: String
-        let height: String
-        let weight: Int
-        let active: Bool
-        let alternateCaptain: Bool
-        let captain: Bool
-        let rookie: Bool
-        let shootsCatches: String
-        let rosterStatus: String
-        let currentTeam: Team
-        let primaryPosition: Position
-    }
     
     let copyright: String
     let people: [PlayerInfo]
