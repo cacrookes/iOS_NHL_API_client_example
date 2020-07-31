4a6f94c04f851864c055e02b744998b65e2b846d
Christopher Crookes <cacrookes@gmail.com>
2020-07-31 15:26:25 -0700

    Adding custom initializer.

--- Udacity Final Project/Model/PlayerInfo.swift
+++ Udacity Final Project/Model/PlayerInfo.swift
@@ -23,7 +23,7 @@ struct PlayerInfo: Codable {
     let birthDate: String
     let currentAge: Int
     let birthCity: String
-    let birthStateProvince: String
+    let birthStateProvince: String?
     let birthCountry: String
     let nationality: String
     let height: String
@@ -36,4 +36,33 @@ struct PlayerInfo: Codable {
     let rosterStatus: String
     let currentTeam: Team
     let primaryPosition: Position
+    
+    init(from decoder: Decoder) throws {
+        let values = try decoder.container(keyedBy: CodingKeys.self)
+        
+        id = try values.decode(Int.self, forKey: .id)
+        fullName = try values.decode(String.self, forKey: .fullName)
+        link = try values.decode(String.self, forKey: .link)
+        firstName = try values.decode(String.self, forKey: .firstName)
+        lastName = try values.decode(String.self, forKey: .lastName)
+        primaryNumber = try values.decode(String.self, forKey: .primaryNumber)
+        birthDate = try values.decode(String.self, forKey: .birthDate)
+        currentAge = try values.decode(Int.self, forKey: .currentAge)
+        birthCity = try values.decode(String.self, forKey: .birthCity)
+        birthCountry = try values.decode(String.self, forKey: .birthCountry)
+        nationality = try values.decode(String.self, forKey: .nationality)
+        height = try values.decode(String.self, forKey: .height)
+        weight = try values.decode(Int.self, forKey: .weight)
+        active = try values.decode(Bool.self, forKey: .active)
+        alternateCaptain = try values.decode(Bool.self, forKey: .alternateCaptain)
+        captain = try values.decode(Bool.self, forKey: .captain)
+        rookie = try values.decode(Bool.self, forKey: .rookie)
+        shootsCatches = try values.decode(String.self, forKey: .shootsCatches)
+        rosterStatus = try values.decode(String.self, forKey: .rosterStatus)
+        currentTeam = try values.decode(PlayerInfo.Team.self, forKey: .currentTeam)
+        primaryPosition = try values.decode(Position.self, forKey: .primaryPosition)
+        
+        // optionals
+        birthStateProvince = try values.decodeIfPresent(String.self, forKey: .birthStateProvince)
+    }
 }
