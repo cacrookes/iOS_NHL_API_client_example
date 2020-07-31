9d9f4679f46dda84df4432291c8717197959e0a2
Christopher Crookes <cacrookes@gmail.com>
2020-07-30 19:44:36 -0700

    Adding Constants file.

---
+++ Udacity Final Project/Constants.swift
@@ -0,0 +1,16 @@
+//
+//  Constants.swift
+//  Udacity Final Project
+//
+//  Created by Christopher Crookes on 2020-07-30.
+//  Copyright © 2020 Christopher Crookes. All rights reserved.
+//
+
+import Foundation
+
+struct K {
+    struct UserDefaultValues {
+        static let hasLaunchedBefore = "hasLaunchedBefore"
+        static let lastUpdateDate = "lastUpdateDate"
+    }
+}
