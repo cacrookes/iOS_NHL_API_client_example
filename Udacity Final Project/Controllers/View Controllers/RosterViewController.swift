8baa5a524db3e4eb4ada93cd8c0243869cd123c9
Christopher Crookes <cacrookes@gmail.com>
2020-08-01 14:36:37 -0700

    Adding RosterViewController.

---
+++ Udacity Final Project/Controllers/View Controllers/RosterViewController.swift
@@ -0,0 +1,30 @@
+//
+//  RosterViewController.swift
+//  Udacity Final Project
+//
+//  Created by Christopher Crookes on 2020-08-01.
+//  Copyright © 2020 Christopher Crookes. All rights reserved.
+//
+
+import UIKit
+
+class RosterViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
