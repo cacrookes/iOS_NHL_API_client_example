ce0325f5b62ad8a6b67cde7e173b9d7bff4d7d1c
Christopher Crookes <cacrookes@gmail.com>
2020-08-01 10:15:21 -0700

    Adding TeamAttributes struct.

---
+++ Udacity Final Project/Model/TeamAttributes.swift
@@ -0,0 +1,46 @@
+//
+//  TeamAttributes.swift
+//  Udacity Final Project
+//
+//  Created by Christopher Crookes on 2020-08-01.
+//  Copyright © 2020 Christopher Crookes. All rights reserved.
+//
+
+import Foundation
+
+struct TeamAttributes {
+    let attributesByTeam = [
+        "ANA": ["primaryHexColour":"#8C2633"],
+        "ARI": ["primaryHexColour":"#8C2633"],
+        "BOS": ["primaryHexColour":"#8C2633"],
+        "BUF": ["primaryHexColour":"#8C2633"],
+        "CAR": ["primaryHexColour":"#8C2633"],
+        "CBJ": ["primaryHexColour":"#8C2633"],
+        "CGY": ["primaryHexColour":"#8C2633"],
+        "CHI": ["primaryHexColour":"#8C2633"],
+        "COL": ["primaryHexColour":"#8C2633"],
+        "DAL": ["primaryHexColour":"#8C2633"],
+        "DET": ["primaryHexColour":"#8C2633"],
+        "EDM": ["primaryHexColour":"#8C2633"],
+        "FLA": ["primaryHexColour":"#8C2633"],
+        "LAK": ["primaryHexColour":"#8C2633"],
+        "MIN": ["primaryHexColour":"#8C2633"],
+        "MTL": ["primaryHexColour":"#8C2633"],
+        "NHL": ["primaryHexColour":"#8C2633"],
+        "NJD": ["primaryHexColour":"#8C2633"],
+        "NSH": ["primaryHexColour":"#8C2633"],
+        "NYI": ["primaryHexColour":"#8C2633"],
+        "NYR": ["primaryHexColour":"#8C2633"],
+        "OTT": ["primaryHexColour":"#8C2633"],
+        "PHI": ["primaryHexColour":"#8C2633"],
+        "PIT": ["primaryHexColour":"#8C2633"],
+        "SJS": ["primaryHexColour":"#8C2633"],
+        "STL": ["primaryHexColour":"#8C2633"],
+        "TBL": ["primaryHexColour":"#8C2633"],
+        "TOR": ["primaryHexColour":"#8C2633"],
+        "VAN": ["primaryHexColour":"#8C2633"],
+        "VGK": ["primaryHexColour":"#8C2633"],
+        "WPG": ["primaryHexColour":"#8C2633"],
+        "WSH": ["primaryHexColour":"#8C2633"]
+    ]
+}
