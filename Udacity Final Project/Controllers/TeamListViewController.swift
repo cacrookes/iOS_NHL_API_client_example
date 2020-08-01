2c9eb8d3746be6cf633e94d2f7b0482f2e8f221b
Christopher Crookes <cacrookes@gmail.com>
2020-07-31 19:03:00 -0700

    Adding TeamListViewController.

---
+++ Udacity Final Project/Controllers/TeamListViewController.swift
@@ -0,0 +1,30 @@
+//
+//  TeamListViewController.swift
+//  Udacity Final Project
+//
+//  Created by Christopher Crookes on 2020-07-31.
+//  Copyright © 2020 Christopher Crookes. All rights reserved.
+//
+
+import UIKit
+
+class TeamListViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
