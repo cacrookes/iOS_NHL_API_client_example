c3fc1caf8a0b26cc23af03b4285603b7c78155fa
Christopher Crookes <cacrookes@gmail.com>
2020-07-31 19:59:25 -0700

    Adding code to display list of teams in table.

--- Udacity Final Project/Controllers/View Controllers/TeamListViewController.swift
+++ Udacity Final Project/Controllers/View Controllers/TeamListViewController.swift
@@ -7,14 +7,83 @@
 //
 
 import UIKit
+import CoreData
 
 class TeamListViewController: UIViewController {
 
+    var dataControler: DataController!
+    var fetchedResultsController: NSFetchedResultsController<Team>!
+    
     @IBOutlet weak var teamListTableView: UITableView!
     
+    // MARK: - View Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        teamListTableView.delegate = self
+        teamListTableView.dataSource = self
+        
+        setupFetchedResultsContainer()
     }
     
+    override func viewDidDisappear(_ animated: Bool) {
+        super.viewDidDisappear(animated)
+        
+        fetchedResultsController = nil
+    }
+    
+    fileprivate func setupFetchedResultsContainer() {
+        let fetchRequest:NSFetchRequest<Team> = Team.fetchRequest()
+        fetchRequest.sortDescriptors = [NSSortDescriptor(key: "name", ascending: true)]
+    
+        fetchedResultsController = NSFetchedResultsController(fetchRequest: fetchRequest, managedObjectContext: dataControler.persistentContainer.viewContext, sectionNameKeyPath: nil, cacheName: "team-list")
+        fetchedResultsController.delegate = self
+        
+        do {
+            try fetchedResultsController.performFetch()
+        } catch {
+            fatalError("The fetch could not be performed.")
+        }
+    }
+}
+
+
+// MARK: - UITableViewDelegate, UITableViewDataSource methods
+extension TeamListViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        let numResults = fetchedResultsController.sections?[section].numberOfObjects ?? 0
+        return numResults
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let team = fetchedResultsController.object(at: indexPath)
+        let cell = tableView.dequeueReusableCell(withIdentifier: K.Identifiers.teamTableViewCell)!
+        cell.textLabel?.text = team.name ?? "N/A"
+        return cell
+    }
+    
+    
+}
+
+// MARK: -
+extension TeamListViewController: NSFetchedResultsControllerDelegate {
+    func controller(_ controller: NSFetchedResultsController<NSFetchRequestResult>, didChange anObject: Any, at indexPath: IndexPath?, for type: NSFetchedResultsChangeType, newIndexPath: IndexPath?) {
+        switch type {
+        case .insert:
+            teamListTableView.insertRows(at: [indexPath!], with: .automatic)
+        default:
+            break
+        }
+    }
+    
+    func controller(_ controller: NSFetchedResultsController<NSFetchRequestResult>, didChange sectionInfo: NSFetchedResultsSectionInfo, atSectionIndex sectionIndex: Int, for type: NSFetchedResultsChangeType) {
+        let indexSet = IndexSet(integer: sectionIndex)
+        switch type{
+        case .insert:
+            teamListTableView.insertSections(indexSet, with: .automatic)
+        default:
+            break
+        }
+    }
+
 }
