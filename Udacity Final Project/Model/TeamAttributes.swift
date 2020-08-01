2d3d32edbb9dbc9c3eff84bb40c22f74ebb1712b
Christopher Crookes <cacrookes@gmail.com>
2020-08-01 10:31:16 -0700

    Adding function to return team's primary colour.

--- Udacity Final Project/Model/TeamAttributes.swift
+++ Udacity Final Project/Model/TeamAttributes.swift
@@ -7,6 +7,7 @@
 //
 
 import Foundation
+import UIKit
 
 struct TeamAttributes {
     let attributesByTeam = [
@@ -43,4 +44,39 @@ struct TeamAttributes {
         "WPG": ["primaryHexColour":"#8C2633"],
         "WSH": ["primaryHexColour":"#8C2633"]
     ]
+    
+    // this function is inspired by:
+    // https://www.hackingwithswift.com/example-code/uicolor/how-to-convert-a-hex-color-to-a-uicolor
+    fileprivate func getUIColorFromHex(_ hex: String) -> UIColor? {
+        let r, g, b, a: CGFloat
+        
+        if hex.hasPrefix("#") {
+            let start = hex.index(hex.startIndex, offsetBy: 1)
+            let hexColor = String(hex[start...])
+            
+            if hexColor.count == 8 {
+                let scanner = Scanner(string: hexColor)
+                var hexNumber: UInt64 = 0
+                
+                if scanner.scanHexInt64(&hexNumber) {
+                    r = CGFloat((hexNumber & 0xff000000) >> 24) / 255
+                    g = CGFloat((hexNumber & 0x00ff0000) >> 16) / 255
+                    b = CGFloat((hexNumber & 0x0000ff00) >> 8) / 255
+                    a = CGFloat(hexNumber & 0x000000ff) / 255
+                    
+                    return UIColor(red: r, green: g, blue: b, alpha: a)
+                    
+                }
+            }
+        }
+        
+        return nil
+    }
+    
+    func getTeamPrimaryColour(forTeamAbbreviation team: String) -> UIColor {
+        let hex = attributesByTeam[team]?["primaryHexColour"] ?? "#ffffff"
+        return getUIColorFromHex(hex) ?? .white
+    }
+    
+
 }
