52312cb31d71c7f00302d518aac4f5b4ea6fbc92
Christopher Crookes <cacrookes@gmail.com>
2020-08-01 15:19:28 -0700

    Wiring up table view to code.

--- Udacity Final Project/Controllers/View Controllers/RosterViewController.swift
+++ Udacity Final Project/Controllers/View Controllers/RosterViewController.swift
@@ -10,21 +10,30 @@ import UIKit
 
 class RosterViewController: UIViewController {
 
+    var dataController: DataController!
+    var team: Team!
+    
+    @IBOutlet weak var rosterTableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        self.title = team.teamName ?? ""
     }
     
 
-    /*
-    // MARK: - Navigation
+}
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+extension RosterViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 0
     }
-    */
-
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: K.Identifiers.rosterTableViewCell)!
+        
+        return cell
+    }
+    
+    
 }
