6b96be52decea784dc8e358c45a7eee166a31e01
Christopher Crookes <cacrookes@gmail.com>
2020-08-01 18:21:55 -0700

    Adding PlayerDetailViewController.

---
+++ Udacity Final Project/Controllers/View Controllers/PlayerDetailViewController.swift
@@ -0,0 +1,30 @@
+//
+//  PlayerDetailViewController.swift
+//  Udacity Final Project
+//
+//  Created by Christopher Crookes on 2020-08-01.
+//  Copyright © 2020 Christopher Crookes. All rights reserved.
+//
+
+import UIKit
+
+class PlayerDetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
